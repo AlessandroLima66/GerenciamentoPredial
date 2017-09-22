890e4b7e7e7cc632428716fc801eba0a73d891ac
Alessandro <ale.limaalexandre@gmail.com>
2017-09-22 19:44:05 -0300

    criando login do usuário

--- app/src/main/java/alecami/arqdesis/usjt/br/gerenciamentopredial/LoginActivity.java
+++ app/src/main/java/alecami/arqdesis/usjt/br/gerenciamentopredial/LoginActivity.java
@@ -1,14 +1,49 @@
 package alecami.arqdesis.usjt.br.gerenciamentopredial;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText login;
+    private EditText senha;
+    ArrayList<Usuario> lista;
+    Intent intent;
+    public static final String CHAVE = "alecami.arqdesis.usjt.br.gerenciamentopredial.chave";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
+        login = (EditText)findViewById(R.id.login_sistema);
+        senha = (EditText) findViewById(R.id.senha_sistema);
+    }
+
+    public void loginSistema(View view){
+        intent = new Intent(this, HomeActivity.class);
+
+        Usuario usuarioLogin = new Usuario(login.toString(), senha.toString());
+        lista = geraListaUsuarios();
+
+        for (int i = 0 ; i < lista.size(); i++){
+            if (lista.get(i).equals( usuarioLogin)){
+                //vai para proxima página
+                startActivity(intent);
+            }
+        }
+        //voltar para página de login
+    }
+
+    public ArrayList<Usuario> geraListaUsuarios() {
+        ArrayList<Usuario> lista = new ArrayList();
+        lista.add(new Usuario("alessandro", "1234"));
+        lista.add(new Usuario("camilla", "1234"));
+        return lista;
     }
 }
